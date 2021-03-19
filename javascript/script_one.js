38a50fe43f430d67cbd46ee8716ec93729d651a2
Euclides-R <euclides.rodriguesjr@outlook.com>
2021-03-18 21:40:30 -0300

    atualization JavaScript, beautiful animations 👨‍💻

--- javascript/script_one.js
+++ javascript/script_one.js
@@ -1,74 +1,54 @@
-//IDENTIFICAR O CLIQUE DO MENU
+'use strict';
+// Link scroll
+
+const corpoItens = document.querySelectorAll('.corpo__itens');
 const menuItens = document.querySelectorAll('.menu a');
 
 menuItens.forEach(item => {
-    item.addEventListener('click', scrollToIdOnClick);
-})
+  item.addEventListener('click', scrollToIdOnClick);
+});
 
 function getScrollTopByHref(element) {
-    const id = element.getAttribute('href');
-    return document.querySelector(id).offsetTop;
+  const id = element.getAttribute('href');
+  return document.querySelector(id).offsetTop;
 }
 
 function scrollToIdOnClick(event) {
-    event.preventDefault();
-    const to = getScrollTopByHref(event.target) - 190;
+  event.preventDefault();
+  const to = getScrollTopByHref(event.target) - 190;
 
-    scrollToPosition(to);
+  scrollToPosition(to);
 }
 
 function scrollToPosition(to) {
-    window.scroll({
-        top: to,
-        behavior: "smooth",
-    });
+  window.scroll({
+    top: to,
+    behavior: 'smooth',
+  });
 }
 
-//IDENTIFICAR QUANDO O USUÁRIO UTILIZA O SCROLL
-//CALCULAR A DISTÂNCIA TOPO DA PÁGINA E O SCROLL
-//CALCULAR A DISTÂNCIA ENTRE O TOPO DA PÁGINA E O ELEMENTO QUE DESEJA ANIMAR
-//COMPARAR AS DUAS DISTÂNCIAS ANTERIORES
-//ADICIONAR UMA CLASSE COM CSS ANIMATION OU TRANSITION AO ELEMENTO ANIMADO
-// Debounce do Lodash
-
-
-debounce = function(func, wait, immediate) {
-    var timeout;
-    return function() {
-        var context = this,
-            args = arguments;
-        var later = function() {
-            timeout = null;
-            if (!immediate) func.apply(context, args);
-        };
-        var callNow = immediate && !timeout;
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
-        if (callNow) func.apply(context, args);
-    };
-};
-
-(function() {
-    var $target = $('.animation'),
-        animationClass = 'animation__start',
-        offSet = $(window).height() * 3 / 4;
-
-    function animationScroll() {
-        var distanciaTop = $(document).scrollTop();
-        $target.each(function() {
-            var itemTop = $(this).offset().top;
-
-            if (distanciaTop > itemTop - offSet) {
-                $(this).addClass(animationClass);
-            } else {
-                $(this).removeClass(animationClass);
-            }
-        })
+//  Animation CSS
+window.addEventListener('scroll', function () {
+  corpoItens.forEach(item => {
+    let itensTop = item.getBoundingClientRect().top;
+    if (window.scrollY > itensTop - 120) {
+      item.classList.add('animation__start');
+      item.classList.remove('animation__end');
+    } else {
+      item.classList.add('animation__end');
+      item.classList.remove('animation__start');
     }
+  });
+});
 
-    animationScroll();
+// Nav fixed
+const menu = document.querySelector('.menu');
+const navFixed = document.getElementById('home').getBoundingClientRect();
 
-    $(document).scroll(debounce(function() {
-        animationScroll();
-    }, 200));
-})();
+window.addEventListener('scroll', function () {
+  if (window.scrollY > navFixed.top) {
+    menu.classList.add('sticky');
+  } else {
+    menu.classList.remove('sticky');
+  }
+});
---
+++ oldScript/script_one.js
@@ -0,0 +1,75 @@
+//IDENTIFICAR O CLIQUE DO MENU
+const menuItens = document.querySelectorAll('.menu a');
+
+menuItens.forEach(item => {
+  item.addEventListener('click', scrollToIdOnClick);
+});
+
+function getScrollTopByHref(element) {
+  const id = element.getAttribute('href');
+  return document.querySelector(id).offsetTop;
+}
+
+function scrollToIdOnClick(event) {
+  event.preventDefault();
+  const to = getScrollTopByHref(event.target) - 190;
+
+  scrollToPosition(to);
+}
+
+function scrollToPosition(to) {
+  window.scroll({
+    top: to,
+    behavior: 'smooth',
+  });
+}
+
+//IDENTIFICAR QUANDO O USUÁRIO UTILIZA O SCROLL
+//CALCULAR A DISTÂNCIA TOPO DA PÁGINA E O SCROLL
+//CALCULAR A DISTÂNCIA ENTRE O TOPO DA PÁGINA E O ELEMENTO QUE DESEJA ANIMAR
+//COMPARAR AS DUAS DISTÂNCIAS ANTERIORES
+//ADICIONAR UMA CLASSE COM CSS ANIMATION OU TRANSITION AO ELEMENTO ANIMADO
+// Debounce do Lodash
+
+debounce = function (func, wait, immediate) {
+  var timeout;
+  return function () {
+    var context = this,
+      args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
+(function () {
+  var $target = $('.animation'),
+    animationClass = 'animation__start',
+    offSet = ($(window).height() * 3) / 4;
+
+  function animationScroll() {
+    var distanciaTop = $(document).scrollTop();
+    $target.each(function () {
+      let itemTop = $(this).offset().top;
+
+      if (distanciaTop > itemTop - offSet) {
+        $(this).addClass(animationClass);
+      } else {
+        $(this).removeClass(animationClass);
+      }
+    });
+  }
+
+  animationScroll();
+
+  $(document).scroll(
+    debounce(function () {
+      animationScroll();
+    }, 200)
+  );
+})();
